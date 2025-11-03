4b9cb2bb4db6a9ade18d462eddecb1daee212fc4
Nitesh Dhakad <110675128+Nitesh1234445@users.noreply.github.com>
2025-11-03 13:07:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let btn = document.querySelector('#incrementBtn');
+let val = document.querySelector('#counter');
+
+btn.addEventListener("click" , ()=>{
+	let currValue = parseInt(val.textContent);
+	alert(currValue + 1);
+	val.innerText = currValue + 1;
+})
